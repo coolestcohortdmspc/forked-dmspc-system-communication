22c1030082555c3097f64f8b111fecdbc5c15724
tmatson-nrao <tmatson@nrao.edu>
2026-08-07 15:00:22 -0400

    Added Name Change Method

--- create_file.py
+++ create_file.py
@@ -1,10 +1,14 @@
 import uuid
 import subprocess
 import random
+import os
+from pathlib import Path
 
 
 def main():
     file_name = f"{uuid.uuid4()}.bin"
+
+    file_path = Path("raw_data") / file_name
 
     subprocess.run("pbcopy", text=True, input=file_name)
 
@@ -12,11 +16,16 @@ def main():
 
     file_size_bytes = file_mb * 1024 * 1024
 
-    with open(file_name, "wb") as file:
+    with open(file_path, "wb") as file:
         for _ in range(20):
             buffer = random.randbytes(int(file_size_bytes / 20))
             file.write(buffer)
 
     print(f"Successfully created a {file_mb}MB random binary file: {file_name}")
+
+
+    # change name method
+    # os.rename(file_path, "./raw_data/receiver_data_" + file_name)
+    # print(f"Successfully created a {file_mb}MB random binary file: receiver_data_" + file_name)
 
 main()
--- read_file.py
+++ read_file.py
@@ -1,8 +1,10 @@
 import sys
 import subprocess
 import time
+from pathlib import Path
 
 
+# lsof method
 while True:
     result = subprocess.run(["lsof", sys.argv[1]], capture_output=True, text=True)
     output = result.stdout
@@ -14,3 +16,24 @@ while True:
     time.sleep(1)
 
 # TODO tell vlba it can start the etransfer!
+
+
+# name change method
+
+# check for files with certain naming structure, i.e. receiver_data_uuid.bin
+# cur_list = list(Path("./raw_data").glob('receiver_data_*'))
+# cur_size = len(cur_list)
+# while True:
+#     new_size = len(list(Path("./raw_data").glob('receiver_data_*')))
+#     if new_size > cur_size:
+#         new_list = list(Path("./raw_data").glob('receiver_data_*'))
+#         new_file = list(set(new_list) - set(cur_list))[0]
+#         print(f"{new_file} is ready for etransfer!")
+#         cur_list.append(new_file)
+#         cur_size = new_size
+#     else:
+#         print("No new files...")
+
+#     time.sleep(1)
+
+# if the number of files increases, tell vlba to start the etransfer
