41e6e445be0d200d68f6c579f6c18df0956de79b
Desmond Lalli <dlalli@NRAO.com>
2026-08-31 14:12:48 -0400

    Implementing SSE  - experiencing issues with synchronous server setup

--- ngRadar_Website/views/views.py
+++ ngRadar_Website/views/views.py
@@ -172,30 +172,34 @@ def serve_image(request, uuid):
 # Othere wise False  
 def lock_status(request):
     logger = logging.getLogger(__name__)
-    try:
-        # UNCOMMENT TO TEST GRACEFUL FAILURE:
-        #raise Exception("TEST CACHE FAILURE")
-        currentStatus = None
-        statusCode = None
-        lock_time = cache.get('submit_locked', None)
-        if lock_time is None:
-            currentStatus = ({"locked": False, "error": False})
-            statusCode=200
-        elif dsocEvent.objects.filter(event_time__gt=lock_time).exists():
-            cache.delete('submit_locked')
-            currentStatus = ({'locked':False, "error": False})
-            statusCode=200
-        else:
-            currentStatus = ({'locked':True, "error": False})
-            statusCode=503
-    except Exception as e:
-        logger.error(f"Cache unavailable while checking submit lock: {e}")
+    while True:
+        # iteration += 1
+        # logger.info(f"Iteration {iteration}")  # Log each cycle
+        try:
+            # UNCOMMENT TO TEST GRACEFUL FAILURE:
+            #raise Exception("TEST CACHE FAILURE")
+            currentStatus = None
+            statusCode = None
+            lock_time = cache.get('submit_locked', None)
+            if lock_time is None:
+                currentStatus = ({"locked": False, "error": False})
+                statusCode=200
+            elif dsocEvent.objects.filter(event_time__gt=lock_time).exists():
+                cache.delete('submit_locked')
+                currentStatus = ({'locked':False, "error": False})
+                statusCode=200
+            else:
+                currentStatus = ({'locked':True, "error": False})
+                statusCode=503
+        except Exception as e:
+            logger.error(f"Cache unavailable while checking submit lock: {e}")
 
-        currentStatus = ({"locked": True, "error": True, "message": "Unable to determine lock status."})
-        statusCode=503
-        
-    yield f"event: lock-status\ndata: {json.dumps(currentStatus)}\n\n"
-    # return JsonResponse(currentStatus, status = statusCode)
+            currentStatus = ({"locked": True, "error": True, "message": "Unable to determine lock status."})
+            statusCode=503
+            
+        yield f"event: lock-status\ndata: {json.dumps(currentStatus)}\n\n"
+        # return JsonResponse(currentStatus, status = statusCode)
+        time.sleep(1)
 
 
 def lock_status_response(request):
