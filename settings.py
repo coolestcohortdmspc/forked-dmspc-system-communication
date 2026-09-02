bc7f689e8ba48cd220cfa4f8f56de89e5916d746
Nicole DePergola <281976283+ndepergo@users.noreply.github.com>
2026-09-02 15:02:14 -0400

    setting fix pytest

--- settings.py
+++ settings.py
@@ -22,12 +22,12 @@ DEBUG = os.environ.get('DJANGO_DEBUG', "False").lower() == "true"
 
 # Allow local Docker containers AND Render's domain depending on environment
 # This setting answers the question: "Is this Host header allowed?"
-ALLOWED_HOSTS = [host.strip() for host in os.environ.get('DJANGO_ALLOWED_HOSTS').split(',') if host.strip()]
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',') if host.strip()]
 
 
 # This setting answers the question: "Is this HTTPS POST allowed to originate from this site?"
 # Without it, our render csrf authentication will fail
-CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS').split(',') if origin.strip()]
+CSRF_TRUSTED_ORIGINS = [origin.strip() for origin in os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost:8000').split(',') if origin.strip()]
 
 
 # Application definition
