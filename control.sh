d7fd4dfaca1d057a2f89b59c20d7ed5757f90637
Anna Kaufmann <akaufman@nrao.edu>
2026-09-03 14:37:10 -0400

    Adding grafana container

--- control.sh
+++ control.sh
@@ -6,7 +6,7 @@ set -a
 source .env
 set +a
 
-START="traefik_http portainer ngradar_website postgres prometheus"
+START="traefik_http portainer ngradar_website postgres prometheus grafana"
 
 DSOC_DROPLET="root@${DSOC_DROPLET_IP}"
 VLBA_1_DROPLET="root@${VLBA_DROPLET_IP}"  # TODO remove when scaling up
@@ -28,7 +28,7 @@ PORTAINER_SERVICE="portainer"
 AGENT_SERVICE="portainer_agent"
 
 # TODO add the commented vlba sims when scaling up! (vlba9 and vlba10 should start before gbt)
-DSOC_SERVICES="traefik ngradar_website postgres prometheus zookeeper kafka-broker kafka-init kafka-ui seaweedfs dsoc-volume-init dsoc etr_daemon"  # vlba7 vlba8
+DSOC_SERVICES="traefik ngradar_website postgres prometheus grafana zookeeper kafka-broker kafka-init kafka-ui seaweedfs dsoc-volume-init dsoc etr_daemon"  # vlba7 vlba8
 VLBA_1_SERVICES="vlba"  # vlba2
 VLBA_2_SERVICES="vlba3 vlba4"
 VLBA_3_SERVICES="vlba5 vlba6"
