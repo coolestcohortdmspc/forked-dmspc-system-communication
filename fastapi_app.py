e0691fb9fb285cf95d7fdb696f1966a9f960e7cc
tnguyen-nrao <tnguyen@nrao.edu>
2026-09-14 11:20:09 -0400

    add signals and topic

--- fastapi_app.py
+++ fastapi_app.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from ngRadar_Website.utils import consume, MAX_BYTES, SESSION_TIMEOUT_MS
+import os
 
 # create instance 
 app = FastAPI()
 
+# module-level variable to store active connections
+active_connections: set[WebSocket]= set()
+
+@app.websocket("/ws/status")
+async def status_ws(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.add(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Process the received data
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
+
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+topic = "obs_status_update"
+config = {
+    "bootstrap.servers": os.environ.get("BOOTSTRAP_SERVERS", "kafka-broker:29092"),
+    "fetch.max.bytes": MAX_BYTES,
+    "session.timeout.ms": SESSION_TIMEOUT_MS,   
+    "client.id": "event-consumer",
+    "group.id": "event-consumer-group",
+    "auto.offset.reset": "earliest",
+}
